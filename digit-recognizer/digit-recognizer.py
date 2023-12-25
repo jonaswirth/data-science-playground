5299399ae3629c87a54e7fe7948fee133516c03e
Jonas <jonas-wirth@gmx.ch>
2023-12-25 13:20:07 +0100

    add dropout

--- digit-recognizer/digit-recognizer.py
+++ digit-recognizer/digit-recognizer.py
@@ -2,11 +2,12 @@ import keras
 import matplotlib.pyplot as plt
 from keras import Sequential
 import numpy as np
-from keras.layers import Input, Activation, Dense
+from keras.layers import Input, Activation, Dense, Dropout
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 train = pd.read_csv('digit-recognizer/data/train.csv')
+X_test = pd.read_csv('digit-recognizer/data/test.csv')
 X_train = train.iloc[:,1:]
 y_train = train.iloc[:,0]
 
@@ -27,8 +28,21 @@ y_train = OneHotEncoder(sparse=False).fit_transform(y_train)
 model = Sequential()
 model.add(Input(shape=(784)))
 model.add(Dense(128, activation="relu"))
+model.add(Dropout(rate=0.2))
+model.add(Dense(128, activation="relu"))
 model.add(Dense(10, activation="softmax"))
 
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(X_train, y_train, epochs=20, batch_size=20, validation_split=.1)
+model.fit(X_train, y_train, epochs=15, batch_size=20, validation_split=.15)
+
+model.save("digit-recognizer/model.keras")
+
+y_pred = model.predict(X_test)
+
+y_pred = np.array(y_pred).argmax(axis=1)
+
+result = pd.DataFrame(range(1, 28001), columns=['ImageId'])
+result = result.assign(label=y_pred)
+
+result.to_csv("digit-recognizer/data/submit.csv", index=False)
